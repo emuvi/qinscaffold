10b38c34a48f84d2c1e2a9fbadc9e82cb604060e
Éverton M. Vieira <everton.muvi@gmail.com>
2022-02-21 16:43:35 -0300

    prototype development

--- build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-bash -v clean.sh
-tsc -p tsconfig.json
--- test-dsk.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-bash -v build.sh
-browserify build/index.js --debug -o public/index.js
-rm -rf ../qinpel-dsk/run/app/qinscaffold
-mkdir ../qinpel-dsk/run/app/qinscaffold
-cd public
-cp -r * ../../qinpel-dsk/run/app/qinscaffold
-cd ..
