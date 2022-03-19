d8cea0345933ee09be267fdbb006e29602e3f9c7
Éverton M. Vieira <everton.muvi@gmail.com>
2022-03-18 22:22:44 -0300

    prototype development

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,42 @@
-import { QinColumn, QinLabel } from "qinpel-cps"
+import {
+  QinLine,
+  QinColumn,
+  QinLabel,
+  QinButton,
+  QinMenu,
+  QinMenuItem,
+  QinIcon,
+  QinAsset,
+} from "qinpel-cps";
+import { QinGrandeur } from "qinpel-res";
 
 class QinScaffold extends QinColumn {
+  private qinTopBar = new QinLine();
+  private qinTest = new QinButton({ label: new QinLabel("Test") });
+  private qinMenu = new QinMenu();
 
-    private qinHello: QinLabel = new QinLabel("Hello, world!");
-
-    public constructor() {
-        super();
-        this.qinHello.install(this);
-    }
-
+  public constructor() {
+    super();
+    this.qinTopBar.install(this);
+    this.qinTopBar.style.putAsFlexMin();
+    this.qinTest.install(this.qinTopBar);
+    this.qinTest.addAction((ev) => {
+      if (ev.isPrimary) {
+        let item = new QinMenuItem(
+          new QinIcon(QinAsset.FaceBag, QinGrandeur.LARGE),
+          new QinLabel("Bag of the multifarios bagged.")
+        );
+        item.addAction((ev) => {
+          if (ev.isSecondary) {
+            item.swapSelect();
+          }
+        });
+        this.qinMenu.putItem(item);
+      }
+    });
+    this.qinMenu.install(this);
+    this.qinMenu.style.putAsFlexMax();
+  }
 }
 
-new QinScaffold().putAsBody();
+new QinScaffold().style.putAsBody();
