f3b68011a03a02f686cdab715063229e93dc84cb
Éverton M. Vieira <everton.muvi@gmail.com>
2022-04-25 11:38:13 -0300

    prototype development

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,14 @@
-import { QinColumn, QinSplitter, QinString } from "qinpel-cps";
+import { QinColumn, QinTable } from "qinpel-cps";
 
 class QinScaffold extends QinColumn {
   public constructor() {
     super();
-    let field1 = new QinString();
-    field1.style.putAsMinWidth(100);
-    let field2 = new QinString();
-    field2.style.putAsMinWidth(100);
-    let splitter = new QinSplitter({sideA: field1, sideB: field2});
-    splitter.install(this);
-    splitter.setVertical();
-    splitter.style.putAsFlexMax();
+    let table = new QinTable();
+    table.install(this);
+    table.setHead(["Name", "Age", "City"]);
+    table.addLine(["John", "25", "New York"]);
+    table.addLine(["Jane", "24", "Paris"]);
+    table.addLine(["Jack", "26", "London"]);
   }
 }
 
