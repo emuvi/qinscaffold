b2c5e0e6fd64acd7554c896a38bd190a0fd906aa
Éverton M. Vieira <emuvi@outlook.com.br>
2022-05-18 13:24:24 -0300

    test qinpel-cps

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,24 @@
-import {
-  QinButton,
-  QinColumn,
-  QinInteger,
-  QinLabel,
-  QinLine,
-  QinString,
-} from "qinpel-cps";
+import { QinButton, QinColumn, QinLabel, QinLine, QinTable } from "qinpel-cps";
 
 class QinScaffold extends QinColumn {
-  private _edit = new QinButton({ label: new QinLabel("Edit") });
-  private _read = new QinButton({ label: new QinLabel("Read") });
-  private _line1 = new QinLine({ items: [this._edit, this._read] });
+  private _button = new QinButton({ label: new QinLabel("Get Row 3") });
+  private _line1 = new QinLine({ items: [this._button] });
 
-  private _int = new QinInteger();
-  private _str = new QinString();
-  private _line2 = new QinLine({ items: [this._int, this._str] });
-
-  private _dialog = new QinButton({ label: new QinLabel("Dialog") });
-  private _line3 = new QinLine({ items: [this._dialog] });
+  private _table = new QinTable();
+  private _line2 = new QinLine({ items: [this._table] });
 
   public constructor() {
     super();
     this._line1.install(this);
     this._line2.install(this);
-    this._edit.addActionMain(() => {
-      this._int.turnEditable();
-      this._str.turnEditable();
-    });
-    this._read.addActionMain(() => {
-      this._int.turnReadOnly();
-      this._str.turnReadOnly();
-    });
-    this._line3.install(this);
-    this._dialog.addActionMain(() => {
-      this.qinpel.jobbed
-        .showDialog("There's a cow on the road.\nShould we stop?")
-        .then((result) => {
-          console.log(result);
-        });
+    this._table.setHead(["Name", "Age", "Address"]);
+    this._table.setLines([
+      ["John", "20", "London"],
+      ["Jane", "20", "London"],
+      ["Jack", "20", "London"],
+    ]);
+    this._button.addActionMain(() => {
+      console.log(this._table.getLine(1));
     });
   }
 }
