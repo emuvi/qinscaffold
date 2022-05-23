681bb3092b519b78c7f8a84426d012e69cefd9f0
Éverton M. Vieira <emuvi@outlook.com.br>
2022-05-23 14:41:44 -0300

    just a build

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,21 @@
-import { QinButton, QinColumn, QinLabel, QinLine, QinTable } from "qinpel-cps";
+import { QinButton, QinColumn, QinLabel, QinLine } from "qinpel-cps";
 
 class QinScaffold extends QinColumn {
-  private _button = new QinButton({ label: new QinLabel("Get Row 3") });
-  private _line1 = new QinLine({ items: [this._button] });
-
-  private _table = new QinTable({ singleSelection: true });
-  private _line2 = new QinLine({ items: [this._table] });
+  private _btnInfo = new QinButton({ label: new QinLabel("Show Info") });
+  private _btnError = new QinButton({ label: new QinLabel("Show Error") });
+  private _line1 = new QinLine({ items: [this._btnInfo, this._btnError] });
 
   public constructor() {
     super();
     this._line1.install(this);
-    this._line2.install(this);
-    this._table.setHead(["Name", "Age", "Address"]);
-    this._table.setLines([
-      ["John", "20", "London"],
-      ["Jane", "20", "London"],
-      ["Jack", "20", "London"],
-      ["John", "20", "London"],
-      ["Jane", "20", "London"],
-      ["Jack", "20", "London"],
-    ]);
-    this._button.addActionMain(() => {
-      let index = Math.floor(Math.random() * 10) % 2 == 0 ? 1 : 2;
-      this._table.select(index);
+    this._btnInfo.addActionMain(() => {
+      this.qinpel.jobbed.showInfo("This is a test");
+    });
+    this._btnError.addActionMain(() => {
+      this.qinpel.jobbed.showError(
+        { why: "This an error with a reason" },
+        "{qinscaffold}(ErrCode-000001)"
+      );
     });
   }
 }
