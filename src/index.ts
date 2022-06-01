f20e173cbd17d6a55bc111afcb917695cfe1280a
Éverton M. Vieira <emuvi@outlook.com.br>
2022-06-01 20:27:41 -0300

    just a build

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 import { QinButton, QinColumn, QinLabel, QinLine } from "qinpel-cps";
 
 class QinScaffold extends QinColumn {
-  private _btnInfo = new QinButton({ label: new QinLabel("Show Info") }).styled({
-    width: "800px",
-  });
+  private _btnInfo = new QinButton({ label: new QinLabel("Show Info") });
   private _btnError = new QinButton({ label: new QinLabel("Show Error") });
   private _line1 = new QinLine({ items: [this._btnInfo, this._btnError] });
 
@@ -11,7 +9,10 @@ class QinScaffold extends QinColumn {
     super();
     this._line1.install(this);
     this._btnInfo.addActionMain(() => {
-      this.qinpel.jobbed.showInfo("This is a test");
+      this.qinpel.jobbed.showInfo(
+        "This is a info with a origin",
+        "{qinscaffold}(ErrCode-000002)"
+      );
     });
     this._btnError.addActionMain(() => {
       this.qinpel.jobbed.showError(
