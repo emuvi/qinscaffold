47d4a8964ca6e9cd98f4e31755bc43519f109b5a
Éverton M. Vieira <emuvi@outlook.com.br>
2022-06-06 17:10:34 -0300

    translates the sentences

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { QinButton, QinColumn, QinLabel, QinLine } from "qinpel-cps";
 
 class QinScaffold extends QinColumn {
-  private _btnInfo = new QinButton({ label: new QinLabel("Show Info") });
-  private _btnError = new QinButton({ label: new QinLabel("Show Error") });
+  private _btnInfo = new QinButton({ label: new QinLabel(this.qinpel.tr("Show Info")) });
+  private _btnError = new QinButton({ label: new QinLabel(this.qinpel.tr("Show Error")) });
   private _line1 = new QinLine({ items: [this._btnInfo, this._btnError] });
 
   public constructor() {
@@ -10,13 +10,13 @@ class QinScaffold extends QinColumn {
     this._line1.install(this);
     this._btnInfo.addActionMain(() => {
       this.qinpel.jobbed.showInfo(
-        "This is a info with a origin",
+        this.qinpel.tr("This is a info with a origin"),
         "{qinscaffold}(ErrCode-000002)"
       );
     });
     this._btnError.addActionMain(() => {
       this.qinpel.jobbed.showError(
-        { why: "This an error with a reason" },
+        { why: this.qinpel.tr("This an error with a reason") },
         "{qinscaffold}(ErrCode-000001)"
       );
     });
